24631b99c3511034046615d921c000b652b14fb8
Marc Hayek <mhayek.90@gmail.com>
2013-11-28 22:07:28 +0200

    adding project files

---
+++ Code/src/cryptoproject/Main.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cryptoproject;
+
+/**
+ *
+ * @author Marc
+ */
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Crypto f=new Crypto();
+        f.setVisible(true);
+//        int k= 143;
+//        int l= 20;
+//        boolean prime=false;
+//        int p1=0;
+//        int p2=0;
+//
+//        for(int g=2;g<k;g++)
+//        {
+//            if (k%g==0)
+//            {
+//                for(int f=2;f<g;f++)
+//                {
+//                    if(g%f==0)
+//                    {   prime=false;
+//                        break;
+//                    }
+//                    else
+//                    {
+//                        prime=true;
+//                    }
+//
+//
+//                }
+//
+//                if(prime==true)
+//                {
+//                    p1=g;
+//                    break;
+//
+//                }
+//
+//
+//            }
+//            }
+//        p2=k/p1;
+//
+//        if(p1<l)
+//        {
+//            System.out.print("GOOsfsdfsD"+ "\r\n");
+//            System.out.print(p1+ "\r\n");
+//            System.out.print(p2);
+//        }
+//     else
+//     {
+//            System.out.print("GOOD"+ "\r\n");
+//
+//     }
+//
+//
+//        }
+    }
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Crypto-Key-Strength-Checker
 ===========================
 
 A small Java application created for my Cryptography course which checks the strength of a password key
+
+Description
+------------
+
+A java application which takes in a set of keys, formed by a product of two prime numbers, and outputs whether they are a strong or bad key.
+The application finds the factors of the keys, and compares them to a give minimum size to check if it is strong or bad.
